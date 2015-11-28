e9637e1738fb00c9447d677f81fc8c9c2ffca168
uTest KenHo <utestken@gmail.com>
2015-11-27 22:28:31 -0500

    Added Android default browser support

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import org.junit.After;
 import org.junit.Assert;
@@ -17,14 +18,26 @@ public class SampleTest {
 
     @Before
     public void setUp() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "iPhone 6");
-        capabilities.setCapability("platformName", "iOS");
-        capabilities.setCapability("platformVersion", "9.1");
-        capabilities.setCapability("browserName", "safari");
+        String device = System.getenv("device");
 
-        this.driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-                capabilities);
+        // switch between diffrent browsers, e.g. iOS Safari or Android Chrome
+        // let's use the os name to differentiate, because we only use default browser in that os
+        if(device.equalsIgnoreCase("Android")){
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("deviceName","Android Emulator");
+            capabilities.setCapability("platformVersion", "4.4");
+            capabilities.setCapability("app", "Browser");
+
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("deviceName", "iPhone 6");
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("platformVersion", "9.1");
+            capabilities.setCapability("browserName", "safari");
+
+            this.driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        }
     }
 
     @Test
